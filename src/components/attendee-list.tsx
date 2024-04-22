1626629fbe2b53d2337b4b96619f1ed9a9309ab4
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-04-22 16:20:04 -0300

    alter

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -42,7 +42,6 @@ export function AttendeeList(){
 
   const [attendees, setAttendees] = useState<Attendee[]>([])
 
-  const totalPages = Math.ceil(attendees.length/10)
 
   useEffect(() => {
     const url = new URL(`http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees?pageIndex=${page - 1}`)
